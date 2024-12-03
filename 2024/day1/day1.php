7c32bb2a1ea52df3b8aa7d6f9aa5d80db89f6ea3
Mert Karayakuplu <mertkarayakuplu@gmail.com>
2024-12-03 07:36:25 +0300

    add solution for part B of day 1
    Signed-off-by: Mert Karayakuplu <mertkarayakuplu@gmail.com>

--- 2024/day1/day1.php
+++ 2024/day1/day1.php
@@ -15,14 +15,29 @@ foreach ($lines as $line) {
 sort($leftSides);
 sort($rightSides);
 
-$totalDistance = 0;
+$totalDistance   = 0;
+$similarityScore = 0;
 
+// super minor optimization of my n^2 :)
+$jl = count($rightSides);
 for ($i = 0, $il = count($leftSides); $i < $il; ++$i) {
     $leftSidePart  = $leftSides[$i];
     $rightSidePart = $rightSides[$i];
     $distance = abs($leftSidePart - $rightSidePart);
     $totalDistance += $distance;
+    $similarInstances = 0;
+
+    for ($j = 0; $j < $jl; ++$j) {
+        $rightSideCand = $rightSides[$j];
+        if ($rightSideCand === $leftSidePart) {
+            ++$similarInstances;
+        }
+    }
+
+    $similarityScore += $similarInstances * $leftSidePart;
 }
 
 echo 'Solution for part A, the total distance: ' . $totalDistance;
+echo "\n";
+echo 'Solution for part B, the total similarity score: ' . $similarityScore;
 echo "\n";
